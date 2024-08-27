afa3c38210a02e412722ee49c7e20efaf2c630f7
Mohamed ALi <mohamed.aly.karim@gmail.com>
2024-08-27 23:26:12 +0300

    AWS

---
+++ app/src/main/java/com/mohalim/edokan/core/model/network/Headers.kt
@@ -0,0 +1,5 @@
+package com.mohalim.edokan.core.model.network
+
+data class Headers(
+    val contentType: String
+)
--- app/src/main/java/com/mohalim/edokan/core/model/network/UserResponse.kt
+++ app/src/main/java/com/mohalim/edokan/core/model/network/UserResponse.kt
@@ -1,9 +1,17 @@
 package com.mohalim.edokan.core.model.network
 
-import com.google.gson.annotations.SerializedName
 import com.mohalim.edokan.core.model.User
 
 data class UserResponse(
-    var message: String,
-    var result: User
+    val statusCode: Int,
+    val body: UserBody,
+    val headers: Headers
 )
+
+data class UserBody(
+    val message: String,
+    val result: User
+)
+
+
+
